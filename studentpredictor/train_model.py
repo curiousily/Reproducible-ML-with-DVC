0aec50c3341f5cbb6286892c8208224c34750321
Venelin Valkov <venelin@curiousily.com>
2020-05-18 23:15:08 +0300

    First experiment

--- studentpredictor/train_model.py
+++ studentpredictor/train_model.py
@@ -1,7 +1,7 @@
 import pickle
 
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import LinearRegression
 
 from config import Config
 
@@ -10,9 +10,7 @@ Config.MODELS_PATH.mkdir(parents=True, exist_ok=True)
 X_train = pd.read_csv(str(Config.FEATURES_PATH / "train_features.csv"))
 y_train = pd.read_csv(str(Config.FEATURES_PATH / "train_labels.csv"))
 
-model = RandomForestRegressor(
-    n_estimators=150, max_depth=6, random_state=Config.RANDOM_SEED
-)
+model = LinearRegression()
 model = model.fit(X_train, y_train.to_numpy().ravel())
 
 pickle.dump(model, open(str(Config.MODELS_PATH / "model.pickle"), "wb"))
